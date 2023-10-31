bfd85069e3548cf0a7dae8ce0299e2b0787f3f90
awaisyaseenSE <148967337+awaisyaseenSE@users.noreply.github.com>
2023-10-31 11:04:20 +0500

    testing

--- src/Screens/Social/Social.js
+++ src/Screens/Social/Social.js
@@ -35,7 +35,7 @@ function Social(props) {
             <View />
             <View />
             <View />
-            <View style={{alignItems: 'center'}}>
+            <View style={{alignItems: 'center', justifyContent: 'center'}}>
               <FastImage
                 source={imagePath.illustration}
                 style={styles.ImageStyle}
